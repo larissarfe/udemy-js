0d01747d1f372420bd8342ff77b739f5ce79b9ec
larissarfe <larissarf.dev@gmail.com>
2023-02-19 16:39:16 -0300

    exercise-0bjects-properties

---
+++ exercise.js
@@ -0,0 +1,9 @@
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'blablabla',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+}
+
+const phase = `${jonas['firstName']} has ${jonas['friends'.length]} friends, and his best friend is ${jonas['friends'[0]]} `;
